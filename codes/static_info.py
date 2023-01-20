ebf6b40d28bee33e312caefa784d807386c7ffe0
Saeed Amiri <samiri@uni-bremen.de>
2023-01-20 15:06:21 +0100

    set constant atomic informations

---
+++ codes/static_info.py
@@ -0,0 +1,42 @@
+class Doc:
+    """All the constant values for types of the partcles"""
+
+
+class AtomMass:
+    """atmoic masses of the particles"""
+    HO: float = 1.0080
+    OB: float = 15.9994
+    OH: float = 15.9994
+    OM: float = 15.9994
+    OMH: float = 15.9994
+    OD: float = 15.9994
+    SI: float = 28.0860
+    SU: float = 28.0860
+    SD: float = 28.0860
+
+
+class AtomCharge:
+    """atomic masses of the particles"""
+    N: float = 0.000
+    HO: float = 0.400
+    OH: float = -0.800
+    SI: float = 1.600
+    OB: float = -0.800
+    SD: float = 1.500
+    OD: float = -1.000
+    OM: float = -0.900
+    OMH: float = -0.900
+    SU: float = 1.200
+
+
+class AtomType:
+    """atomic types of the particles"""
+    HO: int = 1
+    OB: int = 2
+    OH: int = 3
+    OM: int = 4
+    OMH: int = 5
+    OD: int = 6
+    SI: int = 7
+    SU: int = 8
+    SD: int = 9
