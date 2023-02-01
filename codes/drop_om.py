d92c345959be33a9be389461c42c3e901652bf14
Saeed Amiri <samiri@uni-bremen.de>
2023-02-01 07:57:30 +0100

    get OM df

--- codes/drop_om.py
+++ codes/drop_om.py
@@ -8,5 +8,20 @@ class DropOM:
     """drop OM in aminos which have less than three OM atoms
     The extra atoms will drop, and bonds, angles, dihedrals
     must be updated"""
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+                 amino,  # From Get amino, no annotaion for loop import!
+                 si_row: pd.DataFrame,  # One row of si df
+                 del_OM: int  # Number of OM to drop
+                 ) -> None:
+        self.__drop_OM(amino, si_row, del_OM)
+
+    def __drop_OM(self,
+                  amino,  # From Get amino, no annotaion for loop import!
+                  si_row: pd.DataFrame,  # One row of si df
+                  del_OM: int  # Number of OM to drop
+                  ) -> None:
+        """drop OM atoms and update all the rest"""
+        amino_OM: int = 3  # Number of OM in amino data file
+        print(si_row['atom_id'], del_OM)
+        OM_df: pd.DataFrame = amino.Atoms_df[amino.Atoms_df['name'] == 'OM']
+        print(OM_df)
