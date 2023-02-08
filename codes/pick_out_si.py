e4d6e78ecb935969d93a679cd44aa521c8b9fd2a
Saeed Amiri <samiri@uni-bremen.de>
2023-02-08 16:25:20 +0100

    first to pick atom randomly!

--- codes/pick_out_si.py
+++ codes/pick_out_si.py
@@ -13,11 +13,68 @@ class Doc:
 class PickSi:
     """label each si based on thier azimuth and polar angles"""
     def __init__(self,
-                 Si_df: pd.DataFrame  # All the non-body Si in the radius
+                 Si_df: pd.DataFrame,  # All the non-body Si in the radius
+                 diameter: float  # The diameter of the Nanoparticles
                  ) -> None:
-        self.__set_lables(Si_df)
+        self.__de_coverage: float = 3.0  # The desire coverage
+        self.__method: str = 'random'  # random or area
+        self.__set_coverage(Si_df, diameter)
+
+    def __set_coverage(self,
+                       Si_df: pd.DataFrame,  # All the non-body Si in the radiu
+                       diameter: float  # The diameter of the Nanoparticles
+                       ) -> pd.DataFrame:
+        """select the method of randomly setting the coverage by
+        eliminating some of the Si from the data frame or based on the
+        area method."""
+        if self.__method == 'random':
+            print('random sparse the coverag')
+            self.__random_sparse(Si_df, diameter)
+        else:
+            self.__set_lables(Si_df, diameter)
+
+    def __random_sparse(self,
+                        Si_df: pd.DataFrame,  # All the non-body Si in radius
+                        diameter: float  # The diameter of the Nanoparticles
+                        ) -> pd.DataFrame:
+        """sparse the Si randomly"""
+        pass
 
     def __set_lables(self,
-                     Si_df: pd.DataFrame  # All the non-body Si in the radius
+                     Si_df: pd.DataFrame,  # All the non-body Si in the radius
+                     diameter: float  # The diameter of the Nanoparticles
                      ) -> pd.DataFrame:
         """adding label to all the Si in the dataframe"""
+        si_coverage: float = self.__get_coverage(Si_df, diameter)
+        N: int = 112
+        counter: int = 0
+        az_list: list[float] = []
+        for item, row in Si_df.iterrows():
+            label_flag: bool = False
+            theta_i: float = -np.pi
+            for i in range(N):
+                theta = 2*np.pi/N + theta_i
+                if row['azimuth'] < theta and row['azimuth'] >= theta_i:
+                    label_flag = True
+                    counter += 1
+                    az_list.append(i)
+                theta_i = theta
+                if label_flag:
+                    break
+        Si_df['az_lable'] = az_list
+        for i in set(az_list):
+            df_ = Si_df[Si_df['az_lable'] == i]
+            del df_
+
+    def __get_coverage(self,
+                       Si_df: pd.DataFrame,  # All the non-body Si in the radiu
+                       diameter: float  # The diameter of the Nanoparticles
+                       ) -> float:
+        """return the coverage of the selected Si are on the np"""
+        return len(Si_df)/(np.pi*diameter*diameter)
+
+    def __get_si_num(self,
+                     diameter: float  # The diameter of the Nanoparticles
+                     ) -> float:
+        """return number of Si for desire coverage of chains on the np"""
+        return int(np.pi*self.__de_coverage*diameter*diameter) + 1
