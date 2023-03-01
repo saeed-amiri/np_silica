18a14f205a98d987a066c706f9f88f57a2cf042c
Saeed Amiri <samiri@uni-bremen.de>
2023-03-01 15:25:07 +0100

    get box`s volume

--- codes/water_box.py
+++ codes/water_box.py
@@ -1,5 +1,5 @@
 """make water box with packmmol
-Preparing and running input file for packmol
+Preparing and running input file for PACKMOL
 It needs the Radius of the NP, also the size of the box, and number of
 water molecules,
 The output file has such a structure:
@@ -46,3 +46,32 @@ from colors_text import TextColor as bcolors
 
 class InFile:
     """preparing input file for the"""
+    def __init__(self,
+                 radius: float  # Radius of the NP after silanization
+                 ) -> None:
+        self.__prepare_input(radius)
+
+    def __prepare_input(self,
+                        radius: float  # Radius of the NP after silanization
+                        ) -> None:
+        """prepare the input file for the PACKMOL"""
+        self.__get_mols_num(radius)
+
+    def __get_mols_num(self,
+                       radius: float  # Radius of the NP after silanization
+                      ) -> int:
+        """get numbers of molecules based on the valume of the water
+        box"""
+        box_volume: float = self._get_box_valume()
+
+    def _get_box_valume(self) -> float:
+        """calculate the valume of the box including sphere's area"""
+        x_lim: float = stinfo.Hydration.X_MAX - stinfo.Hydration.X_MIN
+        y_lim: float = stinfo.Hydration.Y_MAX - stinfo.Hydration.Y_MIN
+        z_lim: float = stinfo.Hydration.Z_MAX - stinfo.Hydration.Z_MIN
+        box_valume: float = x_lim*y_lim*z_lim
+        if box_valume <= 0:
+            sys.exit(f'{bcolors.FAIL}{self.__class__.__name__}:\n'
+                     f'\tZero valume, there in problem in setting box '
+                     f'limitaion, box_volume is "{box_valume:.3f}"'
+                     f'{bcolors.ENDC}')
