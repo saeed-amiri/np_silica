7b12fe55eefedc206a46e548268b3e9f606527d2
Saeed Amiri <samiri@uni-bremen.de>
2023-03-02 15:27:09 +0100

    read the PDB file for the water_box

---
+++ codes/read_water_box.py
@@ -0,0 +1,54 @@
+"""read water box data, output from PACKMOL package created by
+water_box module.
+The data is in PDB format with bonds and angles written particular
+format.
+The data is set in exact locations.
+atoms:
+    ATOM      1  H   HOH A   1     -24.410  81.001 -67.852
+    ATOM      2  H   HOH A   1     -25.876  80.363 -68.186
+    ATOM      3  O   HOH A   1     -25.259  80.652 -67.455
+The second column of ATOM record are obj, could contains strings, but
+they are same everwher.
+bonds and angles:
+    CONECT    1    3
+    CONECT    2    3
+    CONECT    3    1    2
+format of the atoms section:
+(I changed the HETAM to ATOM in main file of water.pdb)
+Protein Data Bank Format for ATOM:
+    Coordinate Section
+      Record Type	Columns	Data 	Justification	Data Type
+      ATOM 	1-4	“ATOM”	                    	character
+      7-11#	Atom serial number      	right	integer
+      13-16	Atom name	                left*	character
+      17	    Alternate location indicator		character
+      18-20§	Residue name	            right	character
+      22	    Chain identifier		            character
+      23-26	Residue sequence number	    right	integer
+      27	    Code for insertions of residues		character
+      31-38	X orthogonal Å coordinate	right	real (8.3)
+      39-46	Y orthogonal Å coordinate	right	real (8.3)
+      47-54	Z orthogonal Å coordinate	right	real (8.3)
+      55-60	Occupancy	                right	real (6.2)
+      61-66	Temperature factor	        right	real (6.2)
+      73-76	Segment identifier¶	        left	character
+      77-78	Element symbol              right	character
+      79-80	Charge		                        character
+For CONECT:
+    COLUMNS DATA TYPE FIELD DEFINITION
+    ------------------------------------------------------------------
+      1  -  6 Record name "CONECT"
+      7  - 11 Integer serial Atom serial number
+      12 - 16 Integer serial Serial number of bonded atom
+      17 - 21 Integer serial Serial number of bonded atom
+      22 - 26 Integer serial Serial number of bonded atom
+      27 - 31 Integer serial Serial number of bonded atom
+      32 - 36 Integer serial Serial number of hydrogen bonded atom
+      37 - 41 Integer serial Serial number of hydrogen bonded atom
+      42 - 46 Integer serial Serial number of salt bridged atom
+      47 - 51 Integer serial Serial number of hydrogen bonded atom
+      52 - 56 Integer serial Serial number of hydrogen bonded atom
+      57 - 61 Integer serial Serial number of salt bridged atom54
+(ref: https://cdn.rcsb.org/wwpdb/docs/documentation/file-format/
+      PDB_format_Dec_1996.pdf)
+"""
