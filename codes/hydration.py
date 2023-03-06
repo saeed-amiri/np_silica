c52108ea42113e56650019a620d37063305fb68a
Saeed Amiri <samiri@uni-bremen.de>
2023-03-06 16:48:10 +0100

    hydration; standalone

--- codes/hydration.py
+++ codes/hydration.py
@@ -1,13 +1,34 @@
 """add water box for the NP after silanization,
 It gets radius of the silanized NP and prepare a box of water with a
-free space around the origin of the box to put the NP there"""
+free space around the origin of the box to put the NP there.
+It should be able to run separately and within the silanization script
+If executed independently, read the LAMMPS data file, make the box,
+put them together, and write both the LAMMPS data file and the PDB
+files.
+If executed within the silanization script, it needs the data before
+writing into the output file."""
 
+import sys
+import my_tools
 import water_box as wbox
+import write_lmp as wrlmp
+import combine_all as merge
+import read_lmp_data as rdlmp
+import read_water_box as rbox
 
 
 if __name__ == '__main__':
-    moles = wbox.NumberMols(radius=50)
-    in_file = wbox.InFile(radius=50,
+    fname: str = sys.argv[1]
+    nano_p = rdlmp.ReadData(fname)
+    radius: float = my_tools.get_radius(nano_p.Atoms_df)
+    moles = wbox.NumberMols(radius=radius)
+    in_file = wbox.InFile(radius=radius,
                           num_mols=moles.number_mols,
                           edge=moles.edge_cube)
     water_box = wbox.RunPackMol()
+    read_box = rbox.GetWaterDf()
+    combined_box = merge.MergeAll(read_box, nano_p)
+    fout: str  # Name of the outout file
+    fout = f'boxed_{fname}'
+    write_lmp = wrlmp.WriteLmp(combined_box, output=fout)
+    write_lmp.write_lmp()
