7efe9bad52fe5c87347ca4ef48144836b44f9d32
Saeed Amiri <samiri@uni-bremen.de>
2023-03-06 16:20:40 +0100

    update names; update mol id and index of bonds and angles

--- codes/combine_all.py
+++ codes/combine_all.py
@@ -18,9 +18,11 @@ class MergeAll:
         self.Atoms_df: pd.DataFrame  # Merged df of water and nanoparticles
         self.Bonds_df: pd.DataFrame  # Merged df of water and nanoparticles
         self.Angles_df: pd.DataFrame  # Merged df of water and nanoparticles
-        self.Masses_Df: pd.DataFrame  # Merged df of water and nanoparticles
-        self.Atoms_df, self.Bonds_df, self.Angles_df, self.Masses_Df = \
+        self.Masses_df: pd.DataFrame  # Merged df of water and nanoparticles
+        self.Dihedrals_df: pd.DataFrame  # Water does not have dihdrals
+        self.Atoms_df, self.Bonds_df, self.Angles_df, self.Masses_df = \
             self.merge_all(water_box, nano_p)
+        self.Dihedrals_df = nano_p.Dihedrals_df
         self.print_info()
 
     def merge_all(self,
@@ -39,10 +41,10 @@ class MergeAll:
         water_atoms, water_bonds, water_angles, water_masses = \
             self.__update_types(water_box, nano_p)
         # Update atoms id
-        level_up: int = np.max(nano_p.Atoms_df['atom_id'])
-        water_atoms = self.__update_atoms_id(level_up, water_atoms, 'atoms')
-        water_bonds = self.__update_atoms_id(level_up, water_bonds, 'bonds')
-        water_angles = self.__update_atoms_id(level_up, water_angles, 'angles')
+        rise_id: int = np.max(nano_p.Atoms_df['atom_id'])
+        water_atoms = self.__update_atoms_id(rise_id, water_atoms, 'atoms')
+        water_bonds = self.__update_atoms_id(rise_id, water_bonds, 'bonds')
+        water_angles = self.__update_atoms_id(rise_id, water_angles, 'angles')
         # Combine them
         merge_atoms: pd.DataFrame  # Merged df of water and nanoparticles
         merge_bonds: pd.DataFrame  # Merged df of water and nanoparticles
@@ -52,7 +54,10 @@ class MergeAll:
         merge_angles = self.__combine_df(nano_p.Angles_df,
                                          water_angles,
                                          'angles')
-        return merge_atoms, merge_bonds, merge_angles, water_masses
+        merge_masses = self.__combine_df(nano_p.Masses_df,
+                                         water_masses,
+                                         'masses')
+        return merge_atoms, merge_bonds, merge_angles, merge_masses
 
     def __combine_df(self,
                      nano_p_df: pd.DataFrame,  # df of the nanoparticle
@@ -64,16 +69,22 @@ class MergeAll:
         if target == 'atoms':
             columns = ['atom_id', 'mol', 'typ', 'charge', 'x', 'y',
                        'z', 'nx', 'ny', 'nz', 'cmt', 'name']
+            mol_rise: int = np.max(nano_p_df['mol'])
+            water_df['mol'] += mol_rise
         elif target == 'bonds':
             columns = ['typ', 'ai', 'aj', 'cmt', 'name']
         elif target == 'angles':
             columns = ['typ', 'ai', 'aj', 'ak']
+        elif target == 'masses':
+            columns = ['mass', 'typ', 'cmt', 'name', 'b_name']
         w_df: pd.DataFrame = water_df[columns].copy()
         np_df: pd.DataFrame = nano_p_df[columns].copy()
+        index_raise = np.max(nano_p_df.index)
+        w_df.index += index_raise
         return pd.concat([np_df, w_df])
 
     def __update_atoms_id(self,
-                          level_up: int,  # Maximum of atom_id in the atoms_df
+                          rise_id: int,  # Maximum of atom_id in the atoms_df
                           water_df: pd.DataFrame,  # df of the water
                           target: str  # Atoms, Bonds, Angles
                           ) -> pd.DataFrame:
@@ -81,14 +92,14 @@ class MergeAll:
         nanoparticle to each atom id"""
         df_c: pd.DataFrame = water_df.copy()
         if target == 'atoms':
-            df_c['atom_id'] += level_up
+            df_c['atom_id'] += rise_id
         elif target == 'bonds':
-            df_c['ai'] += level_up
-            df_c['aj'] += level_up
+            df_c['ai'] += rise_id
+            df_c['aj'] += rise_id
         elif target == 'angles':
-            df_c['ai'] += level_up
-            df_c['aj'] += level_up
-            df_c['ak'] += level_up
+            df_c['ai'] += rise_id
+            df_c['aj'] += rise_id
+            df_c['ak'] += rise_id
         return df_c
 
     def __update_types(self,
