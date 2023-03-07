78adaa46b6c5c963c95678041375189ea4c01ff0
Saeed Amiri <samiri@uni-bremen.de>
2023-03-07 08:55:49 +0100

    add moving to COM

--- codes/my_tools.py
+++ codes/my_tools.py
@@ -4,6 +4,7 @@ get_radius: find the the radius of the silanized or pure nano partilces
 
 import pandas as pd
 import numpy as np
+from colors_text import TextColor as bcolors
 
 
 def get_radius(atoms_df: pd.DataFrame  # Atoms infos, must contain x,y,z column
@@ -14,3 +15,18 @@ def get_radius(atoms_df: pd.DataFrame  # Atoms infos, must contain x,y,z column
     y_max: float = atoms_df['y'].abs().max()
     z_max: float = atoms_df['z'].abs().max()
     return np.max([x_max, y_max, z_max])
+
+def com_to_zero(atoms_df: pd.DataFrame  # Atoms df
+                ) -> pd.DataFrame:
+    """set the center of mass to zero"""
+    print(f'\n{bcolors.OKCYAN}'
+          '\tMove the center of mass to zero'
+          f'{bcolors.ENDC}')
+    x_cm: float = np.average(atoms_df['x'])
+    y_cm: float = np.average(atoms_df['y'])
+    z_cm: float = np.average(atoms_df['z'])
+    df_c: pd.DataFrame = atoms_df.copy()
+    df_c['x'] -= x_cm
+    df_c['y'] -= y_cm
+    df_c['z'] -= z_cm
+    return df_c
