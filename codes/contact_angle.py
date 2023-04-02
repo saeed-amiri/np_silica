0896bf448f5e479536fbc172f584bcf70e21d848
Saeed Amiri <samiri@uni-bremen.de>
2023-04-02 10:05:38 +0200

    initiate calculations

--- codes/contact_angle.py
+++ codes/contact_angle.py
@@ -6,6 +6,17 @@ If the value for contact angle is a negative number, that means there
 is no oil phase in the system.
 """
 
+import sys
+import numpy as np
+import static_info as stinfo
+from colors_text import TextColor as bcolors
+
+
+def oil_depth(radius: float  # Radius of the nanoparticle
+              ) -> float:
+    """calculate and return the the depth of oil phase `h` in water"""
+    return radius * np.tan(stinfo.Hydration.CONATCT_ANGLE/2)
+
 
 class BoxEdges:
     """make the calculation for system box based on the contact angle"""
@@ -18,6 +29,25 @@ class BoxEdges:
                       radius: float  # Radius of NP after silanization
                       ) -> None:
         """calculate the edges of the water and oil phases"""
+
+
+class NumMols:
+    """calculate the number of molecules for each of the system:
+    Numbers of water molecules (In data files named as: SOL)
+    Number of decane molecules (In data files named as: D10)
+    Numbers of ODAP molecules (In data files named as: ODA)
+    Numbers od ODA molecules (In data files named as: ODN)
+    Numbers of ION atoms (In data files named as: NA or CL)
+    """
+    def __init__(self,
+                 radius: float,  # Radius of the silanized nanoparticle (NP)
+                 net_charge: float  # Charge of the silanized NP with sign!
+                 ) -> None:
+        self.sol_num: int  # Number of water molecules
+        self.d10_num: int  # Number of decane molecules
+        self.oda_num: int  # Number of ODAp molecules
+        self.odn_num: int  # Number of ODA molecules
+        self.ion_num: int  # Number of ion atoms with sign!
 
 
 if __name__ == '__main__':
