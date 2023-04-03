0c41ee8215b8dadc8a934f7a57ba6093ee5a6650
Saeed Amiri <samiri@uni-bremen.de>
2023-04-03 14:52:35 +0200

    DRY

--- codes/hydration.py
+++ codes/hydration.py
@@ -13,9 +13,12 @@ import my_tools
 import water_box as wbox
 import write_lmp as wrlmp
 import combine_pdb as cpdb
+import run_packmol as pakml
 import combine_all as merge
+import static_info as stinfo
 import read_lmp_data as rdlmp
 import read_water_box as rbox
+import box_dimensions as boxd
 import lmp_itp_pdb as itpdb
 
 
@@ -23,18 +26,18 @@ if __name__ == '__main__':
     fname: str = sys.argv[1]
     nano_p = rdlmp.ReadData(fname)
     radius: float = my_tools.get_radius(nano_p.Atoms_df)
-    moles = wbox.NumberMols(radius=radius)
+    dims = boxd.BoxEdges(radius, net_charge=nano_p.Net_charge)
     in_file = wbox.InFile(radius=radius,
-                          num_mols=moles.number_mols,
-                          edge=moles.edge_cube,
-                          net_charge=nano_p.Net_charge)
-    water_box = wbox.RunPackMol()
+                          dimensions=dims)
+    water_box = pakml.RunPackMol(inp_file=stinfo.Hydration.INP_FILE,
+                                 out_file=stinfo.Hydration.OUT_FILE)
     read_box = rbox.GetWaterDf(in_file.num_water)
     combined_box = merge.MergeAll(read_box, nano_p)
     fout: str  # Name of the output file
     fout = f'boxed_{fname}'
     write_lmp = wrlmp.WriteLmp(combined_box, output=fout)
     write_lmp.write_lmp()
-    silica_pdb = itpdb.Call(fname=fname, num_ions=in_file.num_ion)
+    silica_pdb = itpdb.Call(fname=fname, num_ions=dims.num_mols['ion'])
     write_pdb = cpdb.InFile(silaniz_pdb=silica_pdb.pdb_file)
-    cpdb.RunPackMol()
+    pakml.RunPackMol(inp_file=stinfo.Hydration.WS_INP,
+                     out_file=stinfo.Hydration.GRO_PDB)
