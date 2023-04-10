a47d1e2e106d16cac1435c1e6505e5be1a745336
Saeed Amiri <samiri@uni-bremen.de>
2023-04-10 12:03:44 +0200

    write input for the gromacs simulation

---
+++ codes/write_gro_inp.py
@@ -0,0 +1,7 @@
+"""write input files for simulation inputs for GROMACS
+For topology file it needs:
+    - itp of the forcefield (stinfo)
+    - itp of ions, water, oil, nanoparticle if needed
+    - the number of the molecules in [ molecules ] section
+"""
+
