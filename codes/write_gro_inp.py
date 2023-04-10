b8181687f5a3ee3d0ee2a8e042685c5830b3147a
Saeed Amiri <samiri@uni-bremen.de>
2023-04-10 14:49:33 +0200

    ions and salt

--- codes/write_gro_inp.py
+++ codes/write_gro_inp.py
@@ -15,12 +15,14 @@ from colors_text import TextColor as bcolors
 class WriteTop:
     """write topology file"""
     def __init__(self,
-                 mol_nums: dict[str, int]  # Number of each molecule in system
+                 mol_nums: dict[str, int],  # Number of each molecule in system
+                 net_charge: int,  # Total charge of silica system with sign
                  ) -> None:
-        self.write_topo(mol_nums)
+        self.write_topo(mol_nums, net_charge)
 
     def write_topo(self,
-                   mol_nums: dict[str, int]  # Number of each molecule
+                   mol_nums: dict[str, int],  # Number of each molecule
+                   net_charge: int,  # Total charge of silica system with sign
                    ) -> None:
         """writ the data"""
         with open(stinfo.GroInp.TOPFILE,  'w', encoding='utf8') as f_out:
@@ -39,7 +41,7 @@ class WriteTop:
             if mol_nums['odn'] > 0:
                 itp_file = os.path.basename(stinfo.Hydration.ODAN_ITP)
                 self.__write_include(f'./{itp_file}', 'protonate ODA', f_out)
-            self.__write_molecules(mol_nums, f_out)
+            self.__write_molecules(mol_nums, f_out, net_charge)
 
     def __write_include(self,
                         itp_file: str,  # Path of the forcefield
@@ -52,13 +54,14 @@ class WriteTop:
 
     def __write_molecules(self,
                           mol_nums: dict[str, int],  # Number of each molecule
-                          f_out: typing.IO  # Topology file
+                          f_out: typing.IO,  # Topology file
+                           net_charge: int,  # Charge of silica with sign
                           ) -> None:
         """write the molecule section"""
         f_out.write('\n\n[ molecules ]\n')
         f_out.write('; Compound\t\t\t#mols\n')
         ordered_list: list[str]  # Order the list in order to appear in topol
-        ordered_list = ('sol', 'ion', 'oil', 'oda', 'odn')
+        ordered_list = ('sol', 'ion', 'oil', 'oda', 'odn', 'sal')
         for key in ordered_list:
             num = mol_nums[key]
             if num > 0:
@@ -66,14 +69,28 @@ class WriteTop:
                     mol_name = stinfo.PdbMass.water_residue
                     self.__write_mol_line(mol_name, num, f_out)
                 if key == 'ion':
-                    if 1 > 0:
+                    if net_charge > 0:
                         mol_name = stinfo.PdbMass.cl_residue
+                        if mol_nums['sal'] > 0:
+                            num += mol_nums['sal']
                     else:
                         mol_name = stinfo.PdbMass.na_residue
                     self.__write_mol_line(mol_name, num, f_out)
                 if key == 'oil':
                     mol_name = stinfo.PdbMass.oil_residue
                     self.__write_mol_line(mol_name, num, f_out)
+                if key == 'oda':
+                    mol_name = stinfo.PdbMass.odap_residue
+                    self.__write_mol_line(mol_name, num, f_out)
+                if key == 'odn':
+                    mol_name = stinfo.PdbMass.odan_residue
+                    self.__write_mol_line(mol_name, num, f_out)
+                if key == 'sal':
+                    mol_name = stinfo.PdbMass.na_residue
+                    self.__write_mol_line(mol_name, num, f_out)
+                    if mol_nums['ion'] == 0:
+                        mol_name = stinfo.PdbMass.cl_residue
+                        self.__write_mol_line(mol_name, num, f_out)
 
     def __write_mol_line(self,
                          mol_name: str,  # Name of the molecule in itp and pdb
@@ -86,4 +103,4 @@ class WriteTop:
 
 if __name__ == '__main__':
     axis_limits = boxd.BoxEdges(radius=25, net_charge=10)
-    topp = WriteTop(axis_limits.num_mols)
+    topp = WriteTop(axis_limits.num_mols, net_charge=10)
